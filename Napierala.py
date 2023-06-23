da6252f890de7e7102ea7fc62b29f633ab0b1a51
JAIR-VG <vicente.jimenez@uacj.mx>
2023-06-22 23:54:20 -0600

    Se crearon dos nuevas funciones. Una obtiene los indices de las distancias menores y el otro las etiquetas de clase

--- Napierala.py
+++ Napierala.py
@@ -12,6 +12,12 @@ def load_dataset(nfile):
     df = np.loadtxt(nfile,dtype='str')
     return np.asarray(df[:,0:-1],dtype=float), np.asarray(df[:,-1],dtype=int)
 
+def get_indxknn(dstnc,k):
+    return dstnc.argsort()[:k]
+
+def get_labels(ylabel,indx):
+    return(ylabel[indx])
+
 
 #single_point = [3, 4]
 #points = np.arange(20).reshape((10,2))
@@ -29,8 +35,11 @@ X,y = load_dataset('prueba.prn')
 #print(X.shape)
 print(X[0,:])
 distancia = mydist2(X[0,:],X[1:,:])
-print(distancia)
+#print(distancia)
 print(distancia.shape)
 
+print(get_indxknn(distancia,5))
+print(y[get_indxknn(distancia,5)])
+print(get_labels(y,get_indxknn(distancia,5)))
 
 
