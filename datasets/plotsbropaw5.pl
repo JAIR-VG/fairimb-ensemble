515d4ce61e806f87f3f601ed19599c4b4a5177bb
JAIR-VG <vicente.jimenez@uacj.mx>
2023-11-01 14:24:46 -0600

    New files for plotting the kind of samples

--- datasets/plotsbropaw5.pl
+++ datasets/plotsbropaw5.pl
@@ -1,6 +1,6 @@
 reset
-set terminal postscript color enhanced dashed font ",14"
-set output 'paw-5-0.eps'
+set terminal png truecolor font ",14" enhanced
+set output 'paw-5-70.png'
 #set xlabel 'Positive'
 #set ylabel 'Negative'
 set key outside center bottom horizontal
@@ -12,8 +12,8 @@ set style line 5 lc rgb 'red' pt 7 ps 1.5
 set style line 2 lc rgb '#D3D3D3' pt 7 ps 1.5
 
 
-plot 'paw02a-600-5-0-bi-SP.txt' u 1:2 w p ls 3 title 'S',\
-'paw02a-600-5-0-bi-BP.txt' u 1:2 w p ls 4 title 'B',\
-'paw02a-600-5-0-bi-RP.txt' u 1:2 w p ls 5 title 'R',\
-'paw02a-600-5-0-bi-OP.txt' u 1:2 w p ls 1 title 'O',\
-'paw02a-600-5-0-bi-N.txt' u 1:2 w p ls 2 title 'N'
+plot 'paw02a-600-5-70-bi-SP.txt' u 1:2 w p ls 3 title 'S',\
+'paw02a-600-5-70-bi-BP.txt' u 1:2 w p ls 4 title 'B',\
+'paw02a-600-5-70-bi-RP.txt' u 1:2 w p ls 5 title 'R',\
+'paw02a-600-5-70-bi-OP.txt' u 1:2 w p ls 1 title 'O',\
+'paw02a-600-5-70-bi-N.txt' u 1:2 w p ls 2 title 'N'
